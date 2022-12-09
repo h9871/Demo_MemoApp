9ba28e59c3404d3074d8887820df894e31fb288a
unclean <metalyacha@naver.com>
2022-12-09 16:03:41 +0900

    make push View

---
+++ MemoDemo/View/DetailView.swift
@@ -0,0 +1,43 @@
+//
+//  DetailView.swift
+//  MemoDemo
+//
+//  Created by 유현재 on 2022/12/09.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @ObservedObject var memo: Memo
+    
+    @EnvironmentObject var store: MemoStore
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                VStack {
+                    HStack {
+                        Text(memo.content)
+                            .padding()
+                        
+                        Spacer(minLength: 0)
+                    }
+                    
+                    Text(memo.insertDate, style: .date)
+                        .padding()
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+        .navigationTitle("메모보기")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(memo: Memo(content: "Dummy"))
+            .environmentObject(MemoStore())
+    }
+}
